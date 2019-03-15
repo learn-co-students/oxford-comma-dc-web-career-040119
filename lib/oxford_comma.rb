5daa252ec2bc82d0da69775fef6b31274860c464
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 23:17:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,28 @@
-def oxford_comma(array)
-
+def  oxford_comma(array)
+  arr_size = array.size
+  pos_infinity = 1/ 0.0
+  num_range= 4..pos_infinity
+  
+  case arr_size
+  when 1
+    array.join
+  when 2
+      array.join(" and ")
+  when 3
+    array.insert(-2, "and")
+   n_str= array.join( ", ")
+    n_arr= n_str.split
+    n_arr.delete_at(-2)
+    n_arr.insert(-2, "and")
+    n_arr.join(" ")
+    when num_range > 3
+      array.insert(-2, "and")
+   n_str= array.join( ", ")
+    n_arr= n_str.split
+    n_arr.delete_at(-2)
+    n_arr.insert(-2, "and")
+    n_arr.join(" ")
+  else
+    array.join(', ')
+  end
 end
