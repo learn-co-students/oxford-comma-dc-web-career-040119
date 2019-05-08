1fcb9c4b507f7a46a49b42a37da9ea827bd445a0
Angela <malaika.mumba@gmail.com>
2019-05-07 21:00:20 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,18 +1,24 @@
 def oxford_comma(array)
+  if array.length == 1
+   return array.join
 
-end
 
-array = ["kiwi"]
-def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
+    elsif array.length == 2
+#   array[-2] <<
+      array.join (" and ")
+#   array.join
+#   elsif array.length == 1
+#   	array.join
+    elsif array.length > 2
+   # array.to_sentence
+
+   array[-1].prepend "and "
 	array.join(", ")
+#   end
+# end
+#
+ # oxford_comma(array)
   end
-end
 
-oxford_comma(array)
+end
+# oxford_comma(array)
